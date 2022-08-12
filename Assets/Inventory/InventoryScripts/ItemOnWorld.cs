914c3cc01d26d0c3b757a7346698cf91b16cc065
zhangliang322 <lianzhang8099@163.com>
2022-08-12 14:16:19 +0800

    asset

---
+++ Assets/Inventory/InventoryScripts/ItemOnWorld.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemOnWorld : MonoBehaviour
+{
+    public Item thisItem;
+    public Inventory playerInventory;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player")) ;
+        {
+            AddNewItem();
+            Destroy(gameObject);
+        }
+    }
+
+    private void AddNewItem()
+    {
+        //add item
+        if (!playerInventory.itemList.Contains(thisItem))
+        {
+            playerInventory.itemList.Add(thisItem);
+        }
+        else
+        {
+            thisItem.itemHeld += 1;
+        }
+    }
+}
